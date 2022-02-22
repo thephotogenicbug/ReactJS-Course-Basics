743e0225ff3e9a08b6eb213fcad6f064e820013e
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-22 12:05:29 +0530

    22-02-2022 : ReactJS Basics Example
    Difference Between Class and Function Based Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FirstComponent from './ClassComponent1';
+import SecondComponent from './ClassComponent2';
+import FunctionComponent1 from './FunctionComponent1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>This is Parent Component</h1>
+    <FirstComponent />
+    <SecondComponent />
+    <FunctionComponent1 />
     </div>
   );
 }
---
+++ src/FunctionComponent1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FunctionComponent1(){
+    return(
+        <h1>Function Based Component</h1>
+    )
+}
+
+export default FunctionComponent1
